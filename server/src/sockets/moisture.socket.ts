c590da05817969a7ac62b58230fb21a2a1ad3ddc
Elias Renman <elias.renman00@gmail.com>
2023-05-21 16:48:59 +0200

    feat: added get moisture

--- server/src/sockets/moisture.socket.ts
+++ server/src/sockets/moisture.socket.ts
@@ -2,6 +2,7 @@ import { Server, Socket } from "socket.io";
 import { DefaultEventsMap } from "socket.io/dist/typed-events";
 import { SocketHandler } from "./sockets";
 import { emitter } from "../eventemitter";
+import { prisma } from "../prisma";
 
 export class MoistureSocket extends SocketHandler {
   constructor(server: Server) {
@@ -15,5 +16,20 @@ export class MoistureSocket extends SocketHandler {
     socket: Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>
   ): void {
     console.log("Connection established with moisture handler.");
+  }
+
+  public getMoisture() {
+    return prisma.moistureValue.findMany({
+      take: 3,
+      orderBy: {
+        createdAt: "desc",
+      },
+      select: {
+        createdAt: true,
+        id: true,
+        name: true,
+        value: true,
+      },
+    });
   }
 }
