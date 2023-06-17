d9cdcfa7f40bc0ed138b6b5f86031ae4714cede9
Elias Renman <elias.renman00@gmail.com>
2023-06-17 13:27:26 +0200

    feat: enqueueing of moisture sensor

--- server/src/processors/moisture.processor.ts
+++ server/src/processors/moisture.processor.ts
@@ -1,18 +1,28 @@
-// import Queue from "queue";
-
+import Queue from "queue";
 import { Prisma } from "@prisma/client";
 import { iotClient } from "../axios/iot.axios";
 import { MositureRow } from "../cron";
+import { config } from "../config";
 
-// const q = new Queue({ results: [] });
+const queue = new Queue({ results: [] });
+
+queue.start().then((result) => console.log("Successfully started queue"));
+
 export function checkReadingAndEnqueue(rows: MositureRow[]) {
-  // Read config for threshold values
-  // Conclude which sensors need watering.
-  // Enqueue relevant
+  rows.forEach((row) => {
+    // Read config for threshold values
+    const threshold = config.config.moisture.thresholds.lower[row.name];
+    // Conclude which sensors need watering.
+    if (row.value.toNumber() >= threshold) {
+      console.log(`Enqueing ${row.name}`);
+      // Enqueue relevant sensors
+      queue.push(() => process(row));
+    }
+  });
 }
-function enqueue() {}
 
-function process() {
+function process(row: MositureRow) {
+  console.log("Started processing of", row.name);
   // Enable water gate
   // Check moisture level once per second for configured time
   // If the moisture level does not increase a significant amount trigger water-empty alarm and clear queue
